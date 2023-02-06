a45df2b24c48bdabd74d8f879873338e4d315173
Nicolas Raze <nicolaspa.raze@gmail.com>
2023-02-06 14:53:19 +0100

    Fix_Collision_Detection

--- Agent.py
+++ Agent.py
@@ -8,22 +8,21 @@ GAUCHE= 1
 AVANT= 2
 
 class Agent:
-    def __init__(self, velocity, rotation_vel, skin= "Software_Game_Assets\Player_car_final.png", position= (0,0)):
+    def __init__(self, velocity, rotation_angle, skin= "Software_Game_Assets\Player_car_final.png", position= (0,0)):
         self.skin= pygame.image.load(skin)
         self.velocity= velocity
-        self.rotation_vel= rotation_vel
+        self.rotation_angle= rotation_angle
         img= Image.open(skin)
         self.width, self.height= img.size
         self.angle= 0
         self.position= position
-        self.hitbox= self.skin.get_rect(center= self.position)
-        self.hitbox.topleft= self.position
+        self.hitbox= self.skin.get_rect(topleft= self.position)
 
     def rotate(self, isLeft):
         if isLeft:
-            self.angle += self.rotation_vel
+            self.angle += self.rotation_angle
         else:
-            self.angle -= self.rotation_vel
+            self.angle -= self.rotation_angle
         if self.angle > 360:
             self.angle= 0
         return self.rotate_agent_img(self.angle)
@@ -35,7 +34,7 @@ class Agent:
         return new_hitbox
     
     def wall_collision(self, first_point, last_point):  #Dans le Tuple on a (Colonne, Ligne)
-        line_coords= self.bresenham_algorithm(first_point[0], first_point[1], last_point[0], last_point[1])
+        line_coords= self.bresenham_algorithm(first_point, last_point)
         x_in_collision = np.logical_and(line_coords[:,0] >= self.hitbox.left, line_coords[:,0] <= self.hitbox.right)
         y_in_collision = np.logical_and(line_coords[:,1] >= self.hitbox.top, line_coords[:,1] <= self.hitbox.bottom)
         result = np.any(np.logical_and(x_in_collision, y_in_collision))
@@ -54,48 +53,42 @@ class Agent:
             position[0] += self.velocity * math.sin(radian_angle)
             position[1] += self.velocity * math.cos(radian_angle)
         self.position= tuple(position)
-        self.hitbox= self.skin.get_rect(center= self.position)
-        self.hitbox.topleft= self.position
+        self.hitbox= self.skin.get_rect(topleft= self.position)
 
     def select_action():
         pass
         
 
-    def bresenham_algorithm(self, x1, y1, x2, y2):
+    def bresenham_algorithm(self, first_point, last_point):
         coords= np.empty((0,2))
+        new_point= []
+        fp_tuple= None
+        lp_tuple= None
         # Détermination de la direction de la ligne
-        steep = abs(y2 - y1) > abs(x2 - x1)
+        steep = last_point[1] - first_point[1] > 0 or last_point[0] - first_point[0] > 0
         isFlip= False
-        if steep:
-            x1, y1 = y1, x1
-            x2, y2 = y2, x2
         # S'assurer que la ligne se dessine de gauche à droite
-        if x1 > x2:
-            x1, x2 = x2, x1
-            y1, y2 = y2, y1
-        else: 
-            isFlip= True
+        if first_point[0] > last_point[0]:
+            fp_tuple= (last_point[0], last_point[1])
+            lp_tuple= (first_point[0], first_point[1])
         # Initialisation
-        dx = x2 - x1
-        dy = abs(y2 - y1)
+        if fp_tuple is not None and lp_tuple is not None:
+            first_point= fp_tuple
+            last_point= lp_tuple
+        dx = last_point[0] - first_point[0]
+        dy = abs(last_point[1] - first_point[1])
         error = dx / 2
-        ystep = -1 if y1 > y2 else 1
-        y = y1
+        ystep = -1 if first_point[1] > last_point[1] else 1
+        y = first_point[1]
         # Boucle principale
-        for x in range(int(x1), int(x2) + 1):
-            if steep:
-                new_point= [np.float32(x),y]
-                coords= np.append(coords, [new_point], axis= 0)
-            else:
-                new_point= [np.float32(x),y]
-                coords= np.append(coords, [new_point], axis= 0)
+        for x in range(int(first_point[0]), int(last_point[0]) + 1):
+            new_point= [np.float32(x),y]
+            coords= np.append(coords, [new_point], axis= 0)
             error -= dy
             if error < 0:
                 y += ystep
                 error += dx
         # Inversion de la ligne si nécessaire
-        if isFlip:
-            coords= np.fliplr(coords)
         return coords
         
     
--- Environnement.py
+++ Environnement.py
@@ -45,8 +45,8 @@ class Environnement:
 
     def capture_wall_collision(self, agent):
         min_dist= 1000
-        debut_mur= (0,0)
-        fin_mur= (0,0)
+        first_wall= (0,0)
+        last_wall= (0,0)
         target_index= 0
         left_collision= False
         right_collision= False
@@ -54,34 +54,40 @@ class Environnement:
             if min_dist > self.manhattan_distance(agent.position, self.left_bound[i,:]):
                 min_dist= self.manhattan_distance(agent.position, self.left_bound[i,:])
                 target_index= i
-        if target_index > 0:
-            debut_mur= tuple(self.left_bound[target_index-1,:])
-            fin_mur= tuple(self.left_bound[target_index,:])
-        else:
-            debut_mur= tuple(self.left_bound[target_index,:])
-            fin_mur= tuple(self.left_bound[target_index+1,:])
-        left_collision= agent.wall_collision(debut_mur, fin_mur)
-        for i in range(self.right_bound.shape[0]):
-            if min_dist > abs((agent.position[0] +agent.position[1]) - (self.right_bound[i, 0] + self.right_bound[i, 1])):
-                min_dist= abs((agent.position[0] +agent.position[1]) - (self.right_bound[i, 0] + self.right_bound[i, 1]))
-                target_index= i
         if target_index < self.left_bound.shape[0] - 1:
-            debut_mur= tuple(self.left_bound[target_index,:])
-            fin_mur= tuple(self.left_bound[target_index+1,:])
+            first_wall= tuple(self.left_bound[target_index,:])
+            last_wall= tuple(self.left_bound[target_index+1,:])
         else:
-            debut_mur= tuple(self.right_bound[target_index-1,:])
-            fin_mur= tuple(self.right_bound[target_index,:])
-        right_collision= agent.wall_collision(debut_mur, fin_mur)
+            first_wall= tuple(self.left_bound[target_index-1,:])
+            last_wall= tuple(self.left_bound[target_index,:])
+        left_collision= agent.wall_collision(first_wall, last_wall)
+        if not left_collision:
+            if target_index < self.left_bound.shape[0] - 1 and target_index > 0:
+                prev_wall= tuple(self.left_bound[target_index-1,:])
+                left_collision= agent.wall_collision(prev_wall, first_wall)
+        for i in range(self.right_bound.shape[0]):
+            if min_dist > self.manhattan_distance(agent.position, self.right_bound[i,:]):
+                min_dist= self.manhattan_distance(agent.position, self.right_bound[i,:])
+                target_index= i
+        if target_index < self.right_bound.shape[0] - 1:
+            first_wall= tuple(self.right_bound[target_index,:])
+            last_wall= tuple(self.right_bound[target_index+1,:])
+        else:
+            first_wall= tuple(self.right_bound[target_index-1,:])
+            last_wall= tuple(self.right_bound[target_index,:])
+        right_collision= agent.wall_collision(first_wall, last_wall)
+        if not right_collision:
+            if target_index < self.right_bound.shape[0] - 1 and target_index > 0:
+                prev_wall= tuple(self.right_bound[target_index-1,:])
+                right_collision= agent.wall_collision(prev_wall, first_wall)
         return left_collision, right_collision
 
 if __name__ == "__main__":
     env= Environnement()
-    agent= Agent(velocity= 10, rotation_vel= 45, position= ((WIDTH/2) - (PLAYER_WIDTH / 2), HEIGHT - PLAYER_HEIGHT))
+    agent= Agent(velocity= 10, rotation_angle= 45, position= ((WIDTH/2) - (PLAYER_WIDTH / 2), HEIGHT - PLAYER_HEIGHT))
     env.attach_agent(agent)
     run= True
     clock= pygame.time.Clock()
-    #hitbox= agent.rotate(isLeft= True)
-    #WINDOW.blit(agent.skin, hitbox.topleft)
     pygame.display.update()
     while run:
         clock.tick(3)
@@ -90,11 +96,11 @@ if __name__ == "__main__":
         WINDOW.blit(FINISH_LINE, (400, 0))
         if env.left_bound.shape[0] > 1:
             env.draw_walls(isLeft= True)
-        if env.right_bound.shape[0] > 1:
+        if env.right_bound.shape[0] > 2:
             env.draw_walls(isLeft= False)
-            if env.left_bound.shape[0] > 1:
+            if env.left_bound.shape[0] > 2:
                 isLeftCollision, isRightCollision= env.capture_wall_collision(agent)
-                print("Left collision: ", isLeftCollision, "Right collision: ", isRightCollision)
+                print("Left collision: ", isLeftCollision, " || Right collision: ", isRightCollision)
                 agent.movement(isForward= True)  
         pygame.display.update()
 
@@ -104,8 +110,9 @@ if __name__ == "__main__":
                 break     
             if event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:   # Left click
-                    print(pygame.mouse.get_pos())       #Dans le Tuple on a (Colonne, Ligne)
+                    print("Left Wall: ", pygame.mouse.get_pos())       #Dans le Tuple on a (Colonne, Ligne)
                     env.left_bound= np.append(env.left_bound, [pygame.mouse.get_pos()], axis= 0)
                 elif event.button == 3: # Right click
+                    print("Right Wall: ", pygame.mouse.get_pos())       #Dans le Tuple on a (Colonne, Ligne)
                     env.right_bound= np.append(env.right_bound, [pygame.mouse.get_pos()], axis= 0)
     pygame.quit()
