a1f7d72a005d0aaca1b4c7e89b01e4a310186696
Nicolas Raze <nicolaspa.raze@gmail.com>
2023-02-28 19:53:19 +0100

    Migration Python Arcade et GA OK pour GEN#1

--- Agent.py
+++ Agent.py
@@ -1,4 +1,4 @@
-import pygame
+import arcade
 from PIL import Image
 import numpy as np
 import math
@@ -6,131 +6,42 @@ import math
 DROITE= 0
 GAUCHE= 1
 AVANT= 2
+FRAME_RATE= 1/10
 
-class Agent:
+
+class Agent():
     def __init__(self, velocity, rotation_angle, skin= "Software_Game_Assets\Player_car_final.png", position= (0,0)):
-        self.SKIN= pygame.image.load(skin)
-        self.skin= self.SKIN
+        self.sprite= arcade.Sprite(skin, 1)
         self.velocity= velocity
         self.rotation_angle= rotation_angle
-        self.img= Image.open(skin)
-        self.width, self.height= self.img.size
         self.angle= 0
-        self.position= position
-        self.hitbox= self.skin.get_rect()   
-        self.hitbox.center= self.position
-        #self.hitbox= self.rotate_agent_img(self.angle)
+        self.sprite.position= position
         self.strategy= np.random.randint(3, size= 100)
-        #self.strategy= np.ones(100, dtype= int)
-        hitbox_color = (255, 0, 0)
-        self.SURFACE= pygame.Surface(self.hitbox.size, pygame.SRCALPHA)
-        self.hitbox_surface = self.SURFACE
-        self.hitbox_surface.fill(hitbox_color)
-        self.hitbox_surface.set_alpha(50)
-        self.surf = self.hitbox_surface.get_rect()
-        self.surf.center= self.hitbox.center
+        
+    def set_strat_index(self, index):
+        self.action_index= index
 
-    def rotate(self, isLeft):
-        angle= 0
+    def rotate(self, isLeft, point):
         if isLeft:
             self.angle += self.rotation_angle
-            angle= 45
         else:
             self.angle -= self.rotation_angle
-            angle= -45
         if self.angle > 360:
             self.angle= 0
-        self.rotate_agent_img(self.angle)
-    
-    def rotate_agent_img(self, angle= 45):
-        self.skin= pygame.transform.rotate(self.SKIN, angle)
-        self.hitbox= self.skin.get_rect(center= self.position)
-        self.hitbox_surface = pygame.transform.rotate(self.SURFACE, angle)
-        self.surf = self.hitbox_surface.get_rect(center= self.hitbox.center)
-    
-    def wall_collision(self, first_point, last_point):  #Dans le Tuple on a (Colonne, Ligne)
-        line_coords= self.bresenham_algorithm(first_point, last_point)
-        x_in_collision = np.logical_and(line_coords[:,0] >= self.hitbox.left, line_coords[:,0] <= self.hitbox.right)
-        y_in_collision = np.logical_and(line_coords[:,1] >= self.hitbox.top, line_coords[:,1] <= self.hitbox.bottom)
-        result = np.any(np.logical_and(x_in_collision, y_in_collision))
-        if result:
-            return True
-        else:
-            return False
+        self.sprite.angle= self.angle
+        self.sprite.position= point
         
     def movement(self, isForward= True):
         radian_angle= math.radians(self.angle)
-        position= list(self.position)
-        #if isForward:
-        position[0] -= self.velocity * math.sin(radian_angle)
-        self.hitbox.x -= self.velocity * math.sin(radian_angle)
-        position[1] -= self.velocity * math.cos(radian_angle)
-        self.hitbox.y -= self.velocity * math.cos(radian_angle)
-        """else:
-            position[0] += self.velocity * math.sin(radian_angle)
-            self.hitbox.x += self.velocity * math.sin(radian_angle)
-            position[1] += self.velocity * math.cos(radian_angle)
-            self.hitbox.y += self.velocity * math.cos(radian_angle)"""
-        self.position= tuple(position)
-        self.surf.x, self.surf.y= self.hitbox.x, self.hitbox.y
+        self.sprite.center_x += self.velocity * math.sin(radian_angle)
+        self.sprite.center_y += self.velocity * math.cos(radian_angle)
 
     def select_action(self, action):
         if action == DROITE:
-            self.rotate(False)
+            self.rotate(False, self.sprite.position)
         elif action == GAUCHE:
-            self.rotate(True)
+            self.rotate(True, self.sprite.position)
         elif action == AVANT:
-            if self.angle> 90 and self.angle< 270:
-                self.movement(isForward= False)
-            else:
-                self.movement(isForward= True)
+            self.movement()
         return action
-        
-
-    def bresenham_algorithm(self, first_point, last_point):
-        coords= np.empty((0,2))
-        new_point= []
-        fp_tuple= None
-        lp_tuple= None
-        # Détermination de la direction de la ligne
-        steep = last_point[1] - first_point[1] > 0 or last_point[0] - first_point[0] > 0
-        isFlip= False
-        # S'assurer que la ligne se dessine de gauche à droite
-        if first_point[0] > last_point[0] or first_point[1] > last_point[1]:
-            fp_tuple= (last_point[0], last_point[1])
-            lp_tuple= (first_point[0], first_point[1])
-        # Initialisation
-        if fp_tuple is not None and lp_tuple is not None:
-            first_point= fp_tuple
-            last_point= lp_tuple
-        if abs(int(first_point[0]) - int(last_point[0])) >= abs(int(first_point[1]) - int(last_point[1])):
-            dx = last_point[0] - first_point[0]
-            dy = abs(last_point[1] - first_point[1])
-            error = dx / 2
-            ystep = -1 if first_point[1] > last_point[1] else 1
-            # Boucle principale
-            y = first_point[1]
-            for x in range(int(first_point[0]), int(last_point[0]) + 1):
-                new_point= [np.float32(x),y]
-                coords= np.append(coords, [new_point], axis= 0)
-                error -= dy
-                if error < 0:
-                    y += ystep
-                    error += dx
-        else:
-            dx = abs(last_point[0] - first_point[0])
-            dy = last_point[1] - first_point[1]
-            error = dy / 2
-            xstep = -1 if first_point[0] > last_point[0] else 1
-            x= first_point[0]
-            for y in range(int(first_point[1]), int(last_point[1]) + 1):
-                new_point= [x,np.float32(y)]
-                coords= np.append(coords, [new_point], axis= 0)
-                error -= dx
-                if error < 0:
-                    x += xstep
-                    error += dy
-        # Inversion de la ligne si nécessaire
-        return coords
-        
     
